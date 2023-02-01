4fd18197f9b0471b4b3b17011ae6f68b137556de
Marton Cseke <v-mcseke@expediagroup.com>
2023-02-01 08:16:36 +0100

    run WallpaperCalculator from Application

--- src/main/java/com/gscf/assignment/AssignmentApplication.java
+++ src/main/java/com/gscf/assignment/AssignmentApplication.java
@@ -1,14 +1,33 @@
 package com.gscf.assignment;
 
 import com.gscf.assignment.components.WallpaperCalculator;
+import com.gscf.assignment.model.Room;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.List;
 
 @SpringBootApplication
 public class AssignmentApplication {
 
+    private static final Logger logger = LogManager.getLogger(AssignmentApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(AssignmentApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(AssignmentApplication.class, args);
+        WallpaperCalculator wallpaperCalculator = context.getBean(WallpaperCalculator.class);
+
+        Long wallpaperNeeded = wallpaperCalculator.calculateWallpaperNeeded();
+        logger.info(String.format("Number of total square feet of wallpaper: %s", wallpaperNeeded));
+
+        List<Room> rooms = wallpaperCalculator.listCubicShapedRoomsOrderedByWallpaperNeededDesc();
+        logger.info(String.format("All rooms from input that have a cubic shape (order by total needed wallpaper descending): %s", rooms));
+
+        List<Room> roomsAppearingMoreThenOnce = wallpaperCalculator.listRoomsAppearingMoreThenOnce();
+        logger.info(String.format("Rooms from input that are appearing more than once (order is irrelevant): %s", roomsAppearingMoreThenOnce));
+
     }
 }
