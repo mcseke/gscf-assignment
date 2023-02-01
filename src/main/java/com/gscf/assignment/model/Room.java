002c0e468fdc96c34d316eb7f758ef9e981dd6db
Marton Cseke <v-mcseke@expediagroup.com>
2023-02-01 15:12:41 +0100

    use lombok annotations

--- src/main/java/com/gscf/assignment/model/Room.java
+++ src/main/java/com/gscf/assignment/model/Room.java
@@ -1,20 +1,18 @@
 package com.gscf.assignment.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-
-import java.util.Objects;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Data
+@EqualsAndHashCode
+@ToString
+@AllArgsConstructor
 public class Room {
     private Integer length;
     private Integer width;
     private Integer height;
-
-    public Room(Integer length, Integer width, Integer height) {
-        this.length = length;
-        this.width = width;
-        this.height = height;
-    }
 
     private Integer calculateSurfaceArea() {
         return 2 * length * width + 2 * width * height + 2 * height * length;
@@ -22,27 +20,5 @@ public class Room {
 
     public Integer wallpaperNeeded() {
         return calculateSurfaceArea() + Math.min(Math.min(length, width), height);
-    }
-
-    @Override
-    public String toString() {
-        return "Room{" +
-                "length=" + length +
-                ", width=" + width +
-                ", height=" + height +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Room room = (Room) o;
-        return length.equals(room.length) && width.equals(room.width) && height.equals(room.height);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(length, width, height);
     }
 }
